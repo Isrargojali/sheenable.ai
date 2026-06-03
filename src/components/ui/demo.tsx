e0622da3e20c1fac9f92af47e7a496f76e054d8e
mumtazkakakhail <mumtazkakakhail@gmail.com>
2026-06-03 21:04:09 +0500

    updating ui for hero section

--- src/components/ui/demo.tsx
+++ src/components/ui/demo.tsx
@@ -3,8 +3,10 @@ import { HeroGeometric } from "@/components/ui/shape-landing-hero"
 
 function DemoHeroGeometric() {
     return <HeroGeometric badge="Kokonut UI"
-            title1 = "Elevate Your"
-            title2 = "Digital Vision" />
+        title1="Elevate Your"
+        title2="Digital Vision" />
 }
 
 export { DemoHeroGeometric }
+
+
