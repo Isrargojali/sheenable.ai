894c762f89faec3c60fd7d7b594b7af713b10335
mumtazkakakhail <mumtazkakakhail@gmail.com>
2026-07-13 01:05:11 +0500

    now on scroll navbar will avaliable over all sections

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -5,7 +5,7 @@ import { useLocation } from "react-router-dom";
 export default function PageTransition({ children }: { children: ReactNode }) {
   const { pathname } = useLocation();
   return (
-    <div key={pathname} className="animate-fade-in">
+    <div key={pathname} className="animate-page-fade">
       {children}
     </div>
   );
